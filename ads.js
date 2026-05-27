e2f51bb99b307e195e38f239ae6b0cfdd2de0f20
anon <anon@anon.dev>
2026-05-27 13:44:58 -0500

    Use atAsyncOptions array: single invoke.js creates all 5 ads

--- ads.js
+++ ads.js
@@ -1,7 +1,15 @@
-window.atOptions = {
-  'key': '63553ed7d05ed693bec4af3e09d22e4c',
-  'format': 'iframe',
-  'height': 90,
-  'width': 728,
-  'params': {}
-};
+window.atAsyncOptions = [];
+var adKeys = ['63553ed7d05ed693bec4af3e09d22e4c'];
+var units = document.querySelectorAll('.ad-unit');
+units.forEach(function(unit, idx) {
+  var id = 'adc-' + idx;
+  unit.id = id;
+  window.atAsyncOptions.push({
+    'key': adKeys[idx % adKeys.length],
+    'format': 'iframe',
+    'height': 90,
+    'width': 728,
+    'container': id,
+    'params': {}
+  });
+});
