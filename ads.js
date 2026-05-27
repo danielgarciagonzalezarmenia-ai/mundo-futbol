efb4de9e8e027f8c6acc72791fa5532924ecf4d4
anon <anon@anon.dev>
2026-05-27 13:42:46 -0500

    Ad fix: atOptions in head, invoke.js direct in each ad-unit HTML

--- ads.js
+++ ads.js
@@ -1,26 +1,7 @@
-(function() {
-  var key = '63553ed7d05ed693bec4af3e09d22e4c';
-  var units = document.querySelectorAll('.ad-unit');
-  if (!units.length) return;
-  var idx = 0;
-  function loadNext() {
-    if (idx >= units.length) return;
-    var unit = units[idx++];
-    var id = 'ad-container-' + idx;
-    unit.id = id;
-    atOptions = {
-      'key': key,
-      'format': 'iframe',
-      'height': 90,
-      'width': 728,
-      'container': id,
-      'params': {}
-    };
-    var s = document.createElement('script');
-    s.src = 'https://www.highperformanceformat.com/' + key + '/invoke.js';
-    s.onload = loadNext;
-    s.onerror = loadNext;
-    unit.appendChild(s);
-  }
-  loadNext();
-})();
+window.atOptions = {
+  'key': '63553ed7d05ed693bec4af3e09d22e4c',
+  'format': 'iframe',
+  'height': 90,
+  'width': 728,
+  'params': {}
+};
