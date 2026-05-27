1859deb072d27e28bac60d2b4cbfe3116bf572bd
anon <anon@anon.dev>
2026-05-27 13:28:08 -0500

    Fix ads: atOptions in head via ads.js, invoke.js direct in HTML body

--- ads.js
+++ ads.js
@@ -1,10 +1,7 @@
-(function() {
-  var units = document.querySelectorAll('.ad-unit');
-  if (!units.length) return;
-  units.forEach(function(unit) {
-    var s = document.createElement('script');
-    s.src = 'https://pl29569361.effectivecpmnetwork.com/1b/f0/a2/1bf0a27dd67fe35433c34852eed28187.js';
-    s.async = true;
-    unit.appendChild(s);
-  });
-})();
+window.atOptions = {
+  'key': '63553ed7d05ed693bec4af3e09d22e4c',
+  'format': 'iframe',
+  'height': 90,
+  'width': 728,
+  'params': {}
+};
