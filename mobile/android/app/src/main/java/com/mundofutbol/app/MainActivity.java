86e32c21a37b19b1792c33a7cdd27ceb96de09cb
anon <anon@anon.dev>
2026-05-31 12:53:15 -0500

    WebView: todas las URLs cargan dentro de la app, no abre navegador

--- mobile/android/app/src/main/java/com/mundofutbol/app/MainActivity.java
+++ mobile/android/app/src/main/java/com/mundofutbol/app/MainActivity.java
@@ -1,5 +1,29 @@
 package com.mundofutbol.app;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        WebView wv = getBridge().getWebView();
+        if (wv != null) {
+            wv.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    view.loadUrl(url);
+                    return true;
+                }
+            });
+        }
+    }
+}
