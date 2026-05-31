00f562c56b7d6b5629a4e5e9f17e51b79051a949
anon <anon@anon.dev>
2026-05-30 21:18:42 -0500

    Integrate local shield images, replace Wikimedia URLs with local path resolution

--- teams-logos.js
+++ teams-logos.js
@@ -1,100 +1,186 @@
-// Base de datos de escudos de equipos
-// Formato: 'Nombre del Equipo': 'URL del escudo'
-// Se puede agregar cualquier equipo cuando sea necesario
+// Resolución de escudos desde img/escudos/ (LOGO_INDEX en logo-index.js)
+// Aliases para nombres que no coinciden con el nombre del archivo
 
-const TEAM_LOGOS = {
-    // Equipos de La Liga
-    'Real Madrid': 'https://upload.wikimedia.org/wikipedia/en/5/56/Real_Madrid_CF.svg',
-    'Barcelona': 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
-    'Atletico Madrid': 'https://upload.wikimedia.org/wikipedia/en/f/f4/Atletico_Madrid_2017_logo.svg',
-    'Sevilla': 'https://upload.wikimedia.org/wikipedia/en/3/3b/Sevilla_FC_logo.svg',
-    'Valencia': 'https://upload.wikimedia.org/wikipedia/en/c/ce/Valenciacf.svg',
-    'Real Sociedad': 'https://upload.wikimedia.org/wikipedia/en/f/f1/Real_Sociedad_logo.svg',
-    'Villarreal': 'https://upload.wikimedia.org/wikipedia/en/7/70/Villarreal_CF_logo.svg',
-    'Athletic Bilbao': 'https://upload.wikimedia.org/wikipedia/en/9/98/Club_Athletic_Bilbao_logo.svg',
-    'Real Betis': 'https://upload.wikimedia.org/wikipedia/en/1/13/Real_Betis_logo.svg',
-    
-    // Equipos de Premier League
-    'Manchester City': 'https://upload.wikimedia.org/wikipedia/en/e/eb/Manchester_City_FC_badge.svg',
-    'Liverpool': 'https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg',
-    'Manchester United': 'https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg',
-    'Chelsea': 'https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg',
-    'Arsenal': 'https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg',
-    'Tottenham': 'https://upload.wikimedia.org/wikipedia/en/b/b4/Tottenham_Hotspur.svg',
-    'Newcastle': 'https://upload.wikimedia.org/wikipedia/en/5/56/Newcastle_United_Logo.svg',
-    'Aston Villa': 'https://upload.wikimedia.org/wikipedia/en/f/f9/Aston_Villa_FC_crest_%282016%29.svg',
-    
-    // Equipos de Bundesliga
-    'Bayern Munich': 'https://upload.wikimedia.org/wikipedia/commons/1/1b/FC_Bayern_M%C3%BCnchen_logo_%282017%29.svg',
-    'Borussia Dortmund': 'https://upload.wikimedia.org/wikipedia/commons/6/67/Borussia_Dortmund_logo.svg',
-    'RB Leipzig': 'https://upload.wikimedia.org/wikipedia/en/0/04/RB_Leipzig_2014_logo.svg',
-    'Bayer Leverkusen': 'https://upload.wikimedia.org/wikipedia/en/5/55/Bayer_04_Leverkusen_logo.svg',
-    'Eintracht Frankfurt': 'https://upload.wikimedia.org/wikipedia/commons/0/04/Eintracht_Frankfurt_Logo.svg',
-    
-    // Equipos de Serie A
-    'Inter Milan': 'https://upload.wikimedia.org/wikipedia/commons/0/05/FC_Internazionale_Milano_2021.svg',
-    'Juventus': 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Juventus_FC_2017_icon_%28black%29.svg',
-    'AC Milan': 'https://upload.wikimedia.org/wikipedia/commons/d/d0/Logo_of_AC_Milan.svg',
-    'Napoli': 'https://upload.wikimedia.org/wikipedia/commons/2/2d/SSC_Neapel.svg',
-    'Roma': 'https://upload.wikimedia.org/wikipedia/en/f/f7/AS_Roma_logo_%282017%29.svg',
-    
-    // Equipos de Ligue 1
-    'PSG': 'https://upload.wikimedia.org/wikipedia/en/a/a7/Paris_Saint-Germain_F.C..svg',
-    'Marseille': 'https://upload.wikimedia.org/wikipedia/commons/d/d8/Olympique_Marseille_logo.svg',
-    'Lyon': 'https://upload.wikimedia.org/wikipedia/en/a/a0/Olympique_Lyonnais.svg',
-    'Monaco': 'https://upload.wikimedia.org/wikipedia/en/b/ba/AS_Monaco_FC.svg',
-    'Lille': 'https://upload.wikimedia.org/wikipedia/commons/1/13/LOSC_Lille_M%C3%A9tropole_2018_logo.png',
-    
-    // Equipos de Eredivisie
-    'Ajax': 'https://upload.wikimedia.org/wikipedia/commons/7/76/AFC_Ajax_1900.svg',
-    'PSV Eindhoven': 'https://upload.wikimedia.org/wikipedia/commons/2/23/PSV_Eindhoven_2021.svg',
-    'Feyenoord': 'https://upload.wikimedia.org/wikipedia/commons/4/44/Feyenoord_Rotterdam_2021.svg',
-    
-    // Equipos de Primeira Liga
-    'Benfica': 'https://upload.wikimedia.org/wikipedia/en/1/1e/S.L._Benfica_logo.svg',
-    'Porto': 'https://upload.wikimedia.org/wikipedia/commons/2/2e/FC_Porto_logo.svg',
-    'Sporting CP': 'https://upload.wikimedia.org/wikipedia/en/2/26/Sporting_Clube_de_Portugal_logo.svg',
-    
-    // Equipos de Liga MX
-    'America': 'https://upload.wikimedia.org/wikipedia/en/8/82/Club_America_logo.svg',
-    'Chivas': 'https://upload.wikimedia.org/wikipedia/en/4/4e/Club_Deportivo_Guadalajara_logo.svg',
-    'Cruz Azul': 'https://upload.wikimedia.org/wikipedia/en/5/5f/Cruz_Azul_FC_logo.svg',
-    'Pumas': 'https://upload.wikimedia.org/wikipedia/en/6/6c/Club_Universitario_Nacional_logo.svg',
-    'Monterrey': 'https://upload.wikimedia.org/wikipedia/en/6/6d/CF_Monterrey_logo.svg',
-    'Tigres': 'https://upload.wikimedia.org/wikipedia/en/4/4f/Tigres_UANL_logo.svg',
-    
-    // Equipos de Brasileirão
-    'Flamengo': 'https://upload.wikimedia.org/wikipedia/en/2/2e/Clube_de_Regatas_do_Flamengo_logo.svg',
-    'Palmeiras': 'https://upload.wikimedia.org/wikipedia/en/4/41/Sociedade_Esportiva_Palmeiras_-_Logo.svg',
-    'Corinthians': 'https://upload.wikimedia.org/wikipedia/en/4/47/Sport_Club_Corinthians_Paulista_logo.svg',
-    'São Paulo': 'https://upload.wikimedia.org/wikipedia/en/8/86/S%C3%A3o_Paulo_Futebol_Clube_logo.svg',
-    'Santos': 'https://upload.wikimedia.org/wikipedia/en/3/3c/Santos_FC_logo.svg',
-    
-    // Equipos de Copa Colombia
-    'Deportivo Cali': 'https://upload.wikimedia.org/wikipedia/en/0/06/Deportivo_Cali_logo.svg',
-    'Alianza Valledupar': 'https://upload.wikimedia.org/wikipedia/en/5/5e/Alianza_FC_logo.svg',
-    'Millonarios': 'https://upload.wikimedia.org/wikipedia/en/1/10/Millonarios_FC_logo.svg',
-    'Atletico Nacional': 'https://upload.wikimedia.org/wikipedia/en/2/2f/Atl%C3%A9tico_Nacional_logo.svg',
-    'Junior': 'https://upload.wikimedia.org/wikipedia/en/6/6e/Atl%C3%A9tico_Junior_logo.svg',
-    
-    // Equipos de NBA
-    'Oklahoma City Thunder': 'https://upload.wikimedia.org/wikipedia/en/d/d4/Oklahoma_City_Thunder.svg',
-    'San Antonio Spurs': 'https://upload.wikimedia.org/wikipedia/en/a/a2/San_Antonio_Spurs.svg',
-    'Los Angeles Lakers': 'https://upload.wikimedia.org/wikipedia/en/3/3c/Los_Angeles_Lakers_logo.svg',
-    'Golden State Warriors': 'https://upload.wikimedia.org/wikipedia/en/1/10/Golden_State_Warriors_logo.svg',
-    'Boston Celtics': 'https://upload.wikimedia.org/wikipedia/en/8/88/Boston_Celtics.svg',
-    'Miami Heat': 'https://upload.wikimedia.org/wikipedia/en/1/1e/Miami_Heat_logo.svg'
+// Overrides manuales para casos donde el auto-índice tiene conflictos
+const MANUAL_LOGOS = {
+    'vitoria': 'img/escudos/clubes/brasil/vitoria.png',
+    'vitoriaguimaraes': 'img/escudos/clubes/portugal/vitoria.png',
+    'vitoriasc': 'img/escudos/clubes/portugal/vitoria.png',
 };
 
-// Función helper para obtener logo de equipo
-function getTeamLogo(teamName) {
-    return TEAM_LOGOS[teamName] || '';
+const TEAM_ALIASES = {
+    // Argentina
+    'bocajuniors': 'boca',
+    'riverplate': 'river',
+    'racingclub': 'racing',
+    'defensayjusticia': 'defensa',
+    'estudiantesriocuarto': 'estudiantesrc',
+    'independienterivadavia': 'independienteriv',
+    'gimnasiayesgrima': 'gimnasia',
+    'gimnasiayesgrimamendoza': 'gimnasiamendoza',
+    'newellsoldboys': 'newells',
+    'rosariocentral': 'rosariocentral',
+    'uniondesantafe': 'union',
+    'deportivoriera': 'riestra',
+    'clubsarmiento': 'sarmiento',
+    'atleticosarmiento': 'sarmiento',
+    // Spain
+    'atleticomadrid': 'atlmadrid',
+    'athleticbilbao': 'athletic',
+    'realbetis': 'betis',
+    'rayovallecano': 'rayovallecano',
+    'realoviedo': 'realoviedo',
+    // England
+    'tottenhamhotspur': 'tottenham',
+    'newcastleunited': 'newcastle',
+    'westhamunited': 'westham',
+    'wolverhamptonwanderers': 'wolves',
+    'leedsunited': 'leeds',
+    'nottinghamforest': 'nottinghamforest',
+    'manchesterunited': 'manchesterunited',
+    'manchestercity': 'manchestercity',
+    'astonvilla': 'astonvilla',
+    'crystalpalace': 'crystalpalace',
+    // Germany
+    'bayernmunich': 'bayernmunchen',
+    'borussiamonchengladbach': 'bmonchengladbach',
+    'eintrachtfrankfurt': 'eintrachtfrankfurt',
+    'bayerleverkusen': 'bayerleverkusen',
+    '1fcunionberlin': 'unionberlin',
+    'fcstpauli': 'stpauli',
+    'stpauli': 'stpauli',
+    // Italy
+    'intermilan': 'inter',
+    'acmilan': 'milan',
+    'hellasverona': 'hellasverona',
+    // France
+    'parissaintgermain': 'psg',
+    'olympiquemarseille': 'olimpiquemarsella',
+    'olympiquelyon': 'olympiquelyon',
+    'strasbourg': 'racingetrasburgo',
+    'racingetrasburgo': 'racingetrasburgo',
+    // Netherlands
+    'psveindhoven': 'psv',
+    'goaheadeagles': 'goaheadeagles',
+    'scoheerenveen': 'scheerenveen',
+    // Portugal
+    'sportingcp': 'sporting',
+    'sportinglisboa': 'sporting',
+    'casapia': 'casapia',
+    'gilvicente': 'gilvicente',
+    'rioave': 'rioave',
+    'vitoriasc': 'vitoriasc',
+    'vitoriasportclub': 'vitoriasc',
+    // Brazil
+    'saopaulo': 'saopaulo',
+    'atleticomineiro': 'atlmineiro',
+    'atleticoparanaense': 'atlparanaense',
+    'rbbragantino': 'rbbragantino',
+    // Colombia
+    'deportivocali': 'depcali',
+    'atleticonacional': 'atlnacional',
+    'aguilasdoradas': 'aguilasdoradas',
+    'alianzavalledupar': 'alianza',
+    'independientemedellin': 'dim',
+    'internacionalbogota': 'internacionalbogota',
+    'boyacachico': 'boyacachico',
+    // Mexico
+    'guadalajara': 'guadalajara',
+    'clubamerica': 'america',
+    'cruzazul': 'cruzazul',
+    'pumasunam': 'pumas',
+    'atleticoslp': 'atleticosl',
+    'chivas': 'guadalajara',
+    // Chile
+    'colocolo': 'colocolo',
+    'universidadcatolica': 'ucatolica',
+    'universidaddechile': 'udechile',
+    'unionlacalera': 'unionlacalera',
+    'deportesconcepcion': 'deportesconcepcion',
+    'deporteslimache': 'deporteslimache',
+    'cobresal': 'cobresal',
+    // Peru
+    'alianzalima': 'alianzalima',
+    'sportingcristal': 'sportingcristal',
+    'deportivogarcilaso': 'deportivogarcilaso',
+    'comerciantesunidos': 'comerciantesunidos',
+    'juanpabloiicollege': 'juanpabloiicollege',
+    'loschankas': 'loschankas',
+    'sportboys': 'sportboys',
+    'atleticograu': 'atleticograu',
+    // Ecuador
+    'independientedelvalle': 'independientedv',
+    'ligadequito': 'ligadequito',
+    'guayaquilcity': 'guayaquilcity',
+    // Uruguay
+    'penarol': 'penarol',
+    'nacional': 'nacional',
+    'montevideowanderers': 'wanderers',
+    'liverpooluruguay': 'liverpooluruguay',
+    'liverpoolmaldonado': 'liverpooluruguay',
+    // USA/Canada
+    'losangelesfc': 'losangeles',
+    'newyorkredbulls': 'newyork',
+    'newyorkcityfc': 'newyorkcity',
+    'realsaltlake': 'realstaltlake',
+    'stlouiscity': 'stlouiscity',
+    'sanjoseearthquakes': 'sanjose',
+    'intermiami': 'intermiami',
+    'dcunited': 'dcunited',
+    'houstondynamo': 'houstondynamo',
+    'orlandocity': 'orlandocity',
+    // Belgium
+    'clubbrujas': 'clubbrujas',
+    'cerclebrugge': 'cerclebrugge',
+    'sttruidense': 'sinttruidense',
+    'unionsaintgilloise': 'unionsaintgilloise',
+    'royalantwerp': 'royalantwerp',
+    'standardlieja': 'standardlieja',
+    'fcvdender': 'fcvdender',
+    // Other countries
+    'estadosunidos': 'usa',
+    'unitedstates': 'usa',
+    'unitedstatesofamerica': 'usa',
+    'coreadelsur': 'coreadelsur',
+    'southkorea': 'coreadelsur',
+    'korearepublic': 'coreadelsur',
+    'arabiasaudita': 'arabiasaudita',
+    'saudiarabia': 'arabiasaudita',
+    'costarica': 'costarica',
+    'nuevazelanda': 'nuevazelanda',
+    'newzealand': 'nuevazelanda',
+    'paisesbajos': 'paisesbajos',
+    'netherlands': 'paisesbajos',
+    'holland': 'paisesbajos',
+    'republicadominicana': 'republicadominicana',
+    'irlandadelnorte': 'irlandadelnorte',
+    'northernireland': 'irlandadelnorte',
+    'costademarfil': 'costademarfil',
+    'ivorycoast': 'costademarfil',
+    'cotedivoire': 'costademarfil',
+    'caboverde': 'caboverde',
+    'capeverde': 'caboverde',
+};
+
+function normTeamName(name) {
+    return String(name).toLowerCase()
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .replace(/[^a-z0-9]/g, '');
 }
 
-// Genera HTML para logo con fallback a escudo genérico
+function getTeamLogo(teamName) {
+    var key = normTeamName(teamName);
+    var lookup = TEAM_ALIASES[key] || key;
+    if (typeof LOGO_INDEX !== 'undefined') {
+        return MANUAL_LOGOS[lookup] || LOGO_INDEX.clubes[lookup] || LOGO_INDEX.selecciones[lookup] || LOGO_INDEX.mundial2026[lookup] || '';
+    }
+    return '';
+}
+
 function teamLogoHtml(teamName) {
-    const url = getTeamLogo(teamName);
-    const safe = String(teamName).replace(/[&<>"']/g, function(c) {
+    var url = getTeamLogo(teamName);
+    var safe = String(teamName).replace(/[&<>"']/g, function(c) {
         return {'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c];
     });
     if (url) {
